a6e8d68c564878808869872a850aec6a059844f0
dlerch <dlerch@gmail.com>
2017-07-29 00:26:34 +0200

    begin main loop

--- hvac.py
+++ hvac.py
@@ -15,26 +15,36 @@ import modbus_tk.defines as mb_def
 import modbus_tk.modbus_rtu as mb_rtu
 import urllib2
 import base64
+import signal
 from time import gmtime, strftime
 from pprint import pprint
 from struct import unpack, pack
 
 
 
-BAUDRATE=9600
-SERIALPORT="/dev/ttyUSB0"
-USERNAME='admin'
-PASSWORD='opendomo'
+# -- GLOBALS --
 
-# -- MODBUS --
+# modbus channel conf
+MB_BAUDRATE=9600
+MB_SERIAL="/dev/ttyUSB0"
+MB_PARITY=serial.PARITY_NONE
+MB_STOPBITS=serial.STOPBITS_ONE
+
+# odcontrol conf
+ODC_USERNAME='admin'
+ODC_PASSWORD='opendomo'
+
+# hvac donf
+ENDLESS_SCREW_LOADING_TIME=10
+ENDLESS_SCREW_WAITING_TIME=60
+
 
 def init_mb_serial():
     ser = serial.Serial(
-        port=SERIALPORT,
-        baudrate=BAUDRATE,
-        parity=serial.PARITY_NONE,
-        #stopbits=serial.STOPBITS_TWO
-        stopbits=serial.STOPBITS_ONE
+        port=MB_SERIAL,
+        baudrate=MB_BAUDRATE,
+        parity=MB_PARITY,
+        stopbits=MB_STOPBITS
     )
     mb = mb_rtu.RtuMaster(ser)
     mb.set_timeout(1)
@@ -153,7 +163,7 @@ def set_value(name, value):
         sys.exit(0)
 
     request = urllib2.Request("http://192.168.1.77:81/set+"+name+'+'+value)
-    base64string = base64.b64encode('%s:%s' % (USERNAME, PASSWORD))
+    base64string = base64.b64encode('%s:%s' % (ODC_USERNAME, ODC_PASSWORD))
     request.add_header("Authorization", "Basic %s" % base64string)   
     result = urllib2.urlopen(request)
     data=result.read().split('\n')
@@ -162,14 +172,20 @@ def set_value(name, value):
 
 def get_value(name):
     request = urllib2.Request("http://192.168.1.77:81/lsc+"+name)
-    base64string = base64.b64encode('%s:%s' % (USERNAME, PASSWORD))
+    base64string = base64.b64encode('%s:%s' % (ODC_USERNAME, ODC_PASSWORD))
     request.add_header("Authorization", "Basic %s" % base64string)   
     result = urllib2.urlopen(request)
     data=result.read().split('\n')
     field=data[0].split(':')
     return field[2]
 
-
+def set_interval(func, sec):
+    def func_wrapper():
+        set_interval(func, sec)
+        func()
+    t = threading.Timer(sec, func_wrapper)
+    t.start()
+    return t
 
 
 # -- UTILS --
@@ -177,8 +193,12 @@ def get_value(name):
 def tm():
     return "["+strftime("%Y-%m-%d %H:%M:%S")+"]"
 
+def debug(string):
+    print tm()+" "+string
+
 ser, mb = init_mb_serial()
 
+"""
 #print read_temperature("HOME_UP")
 #print read_temperature("TERMO")
 #print "C1_1:", read_temperature("C1_1")
@@ -190,13 +210,50 @@ print "INERCIA:", read_temperature("INERCIA")
 print "ACS:", read_temperature("ACS")
 print "S2:", read_temperature("S2")
 print "--"
+"""
+
+# HVAC SYSTEM
+
+def stop(signum, frame):
+        print 'exit ...' 
+        set_value("TERMO", "OFF"); print "TERMO:", get_value("TERMO")
+        set_value("BmC01", "OFF"); print "BmC01:", get_value("BmC01")
+        set_value("BmC02", "OFF"); print "BmC02:", get_value("BmC02")
+        set_value("BmACS", "OFF"); print "BmACS:", get_value("BmACS")
+        set_value("SFdep", "OFF"); print "SFdep:", get_value("SFdep")
+        set_value("SFqum", "OFF"); print "SFqum:", get_value("SFqum")
+        set_value("VENTL", "OFF"); print "VENTL:", get_value("VENTL")
+        set_value("BmCAL", "OFF"); print "BmCAL:", get_value("BmCAL")
+        sys.exit(0)
+
+def endless_screw():
+    debug("endless_screw(): set on")
+    set_value("SFqum", "ON"); 
+    set_value("SFdep", "ON"); 
+    debug("endless_screw(): loading biomass")
+    time.sleep(ENDLESS_SCREW_LOADING_TIME)
+    debug("endless_screw(): set off")
+    set_value("SFqum", "OFF"); 
+    set_value("SFdep", "OFF"); 
 
 
-set_value("BmC01", "ON")
-print get_value("BmC01")
 
-set_value("BmC01", "OFF")
-print get_value("BmC01")
+signal.signal(signal.SIGINT, stop)
+
+while True:
+    time.sleep(0.5)
+    t=int(time.time())
+    #print t%ENDLESS_SCREW_WAITING_TIME
+
+    if t%ENDLESS_SCREW_WAITING_TIME==0:
+        endless_screw()
+
+
+#set_value("BmC01", "ON")
+#print get_value("BmC01")
+
+#set_value("BmC01", "OFF")
+#print get_value("BmC01")
 
 
 #print "[1] ERR CH1 CH2:", mb.execute(1, 3, 25-1, 1)
@@ -207,21 +264,6 @@ print get_value("BmC01")
 
 sys.exit(0)
 
-
-try:
-    print "-- ALL 1 --"
-    print mb.execute(1, 3, 0, 16)
-    print mb.execute(1, 3, 16, 16)
-    print mb.execute(1, 3, 32, 13)
-    print "-- ALL 2 --"
-    print mb.execute(2, 3, 0, 16)
-    print mb.execute(2, 3, 16, 16)
-    print mb.execute(2, 3, 32, 13)
-except:
-    print "ERROR:", traceback.format_exc()
-
-sys.stdout.flush()
-ser.close()
 
 
 
